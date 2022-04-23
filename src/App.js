f0eaad4d6ec9c0e37c316b96250b6b3b7ef775a1
bimbellb3r <bimbellb3r@gmail.com>
2022-04-23 23:25:51 +0800

    styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import NavbarComponent from './Component/NavbarComponent';
-import './App.css';
+import "./App.css";
+import { Col, Container, Row } from "react-bootstrap";
+import { Hasil, ListCategories, NavbarComponent } from "./Component";
 
 function App() {
   return (
     <div className="App">
-      <NavbarComponent/>
+      <NavbarComponent />
+      <div className="mt-3">
+        <Container fluid>
+          <Row>
+            <ListCategories />
+            <Col>
+              <h4>
+                <strong>Daftar Produk</strong>
+              </h4>
+              <hr />
+            </Col>
+            <Hasil />
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 }
